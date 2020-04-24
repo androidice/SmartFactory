8ae50613ceb5b7c0995bf8c8fccd9de4a79145e8
kevin <kevinalviola@gmail.com>
2020-04-25 00:30:53 +0800

    initial project structure

---
+++ Innexus.SmartFactory.Web/Data/SFDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Innexus.SmartFactory.Web.Data
+{
+    public class SFDbContext : IdentityDbContext
+    {
+        public SFDbContext(DbContextOptions<SFDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
